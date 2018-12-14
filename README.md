66988021225efa9d541d192c2ab80d782c225715
Jesper Öqvist <jesper.oqvist@cs.lth.se>
2018-12-14 07:07:54 +0100

    Add --output and --by options
    * Redesigned --by-name into --by=name.
    * Changed alias file format.
    * Improved TeX output.
    * Added CSV output.

--- README.md
+++ README.md
@@ -1,9 +1,20 @@
 # Git LineCount
 
-Simple cumulative line count per author tool for git in Python 2.
+A tool for counting cumulative changes per author for Git repositories.
+Can output text, CSV, or LaTeX.
 
-This program sums the output from `git show --numstat` to actual number of
+This program sums the output from `git show --numstat` to get actual number of
 inserted/removed lines, unlike other line counting tools which use `--stat` to
 estimate line counts (good luck with that, lol).
 
-The source is currently the only documentation.
+Usage:
+
+1. `python countlines.py <path-to-repo> --by=name --output=tex > stats.tex`
+2. `xelatex stats.tex`
+
+Usage instructions (with alias file):
+
+1. Generate an initial alias file:
+    `python countlines.py <path-to-repo> --output=alias > aliasfile`
+2. Edit `aliasfile`
+3. `python countlines.py <path-to-repo> --alias=aliasfile --by=name --output=csv > stats.csv`
--- countlines.py
+++ countlines.py
@@ -1,6 +1,6 @@
+#!/usr/bin/env python3
 # Copyright (c) 2018, Jesper Öqvist
 import argparse
-import ConfigParser
 import sys, os, subprocess
 import re
 
@@ -12,12 +12,23 @@ class Author:
         self.added = 0
         self.deleted = 0
 
-    def __str__(s):
-        return '%s & %d & %d & %d' % (s.name, s.commits, s.added, s.deleted)
+    def report(s, by, sep, suffix=''):
+        metrics = [str(s.commits), str(s.added), str(s.deleted), str(s.added + s.deleted)]
+        if by == 'name':
+            return sep.join([s.name] + metrics) + suffix
+        elif by == 'email':
+            return sep.join([s.email] + metrics) + suffix
+        else:
+            return sep.join(["%s <%s>" % (s.name, s.email)] + metrics) + suffix
 
-def index(authors, name, email, by_name):
-    if by_name:
+    def __str__(s):
+        return '%s <%s>' % (s.name, s.email)
+
+def index(authors, name, email, by):
+    if by == 'name':
         key = name
+    elif by == 'email':
+        key = email
     else:
         key = (name, email)
     if not key in authors:
@@ -28,14 +39,20 @@ def main():
     authors = {}
     parser = argparse.ArgumentParser(description='Counts lines in git project.')
     parser.add_argument('repository', help='The git repository to count lines in.')
-    parser.add_argument('--by-name', dest='by_name', action='store_true',
-            help='Collate by author name (ignoring email)')
-    parser.add_argument('--alias', help='Author name alias file')
+    parser.add_argument('--by',
+            choices=['name', 'email', 'both'],
+            default='both',
+            help='Collate by author name, email, or both')
+    parser.add_argument('--output', dest='output',
+            choices=['plaintext', 'tex', 'csv', 'alias'],
+            default='plaintext',
+            help='Output as TeX document')
+    parser.add_argument('--alias', help='File mapping emails to author names')
     args = parser.parse_args()
     repo = args.repository
     if not os.path.isdir(repo):
         # Check out the repo to local directory.
-        print 'Cloning %s into ./repo' % repo
+        print('Cloning %s into ./repo' % repo)
         if not os.path.isdir('repo'):
             exit_code = subprocess.call(['git', 'clone', repo, 'repo'])
             if exit_code != 0:
@@ -47,36 +64,60 @@ def main():
     if args.alias:
         with open(args.alias, 'r') as fp:
             for ln in fp.readlines():
-                pseudonym, name = ln.split('=')
-                alias[pseudonym.strip()] = name.strip()
+                email, name = ln.split('=')
+                alias[email.strip()] = name.strip()
 
     # Process commits.
     os.chdir(repo)
     revs = subprocess.check_output(['git', 'rev-list', 'master']).split()
     re_auth = re.compile('^(.+)#(.*)')
     re_edits = re.compile('^(\d+)\s+(\d+)') # Binary files start with - -.
+    N = len(revs)
     n = 0
     for rev in revs:
         n += 1
-        sys.stderr.write('\rcommit: %d%s' % (n, ' '*30))
+        sys.stderr.write('\rcommit: %d / %d%s' % (n, N, ' '*15))
         out,err = subprocess.Popen(['git', 'show', rev, '--numstat', '--format=%an#%ae'], stdout=subprocess.PIPE).communicate()
+        out = out.decode(encoding='UTF-8')
         lines = out.splitlines()
         auth_line = lines[0]
         auth = re_auth.match(auth_line)
         if not auth:
             raise Exception('Malformed author line? [%s]' % auth_line)
         name,email = auth.group(1), auth.group(2)
-        auth = index(authors, alias.get(name, name), email, args.by_name)
+        auth = index(authors, alias.get(email, name), email, args.by)
         auth.commits += 1
         for ln in lines[1:]:
             edits = re_edits.match(ln)
             if edits:
                 auth.added += int(edits.group(1))
                 auth.deleted += int(edits.group(2))
-    sys.stderr.write('\rdone%s\n' % (' ' * 30))
+    sys.stderr.write('\r%s\r' % (' ' * 25))
     authors = sorted(authors.values(), key=lambda x: x.added + x.deleted, reverse=True)
-    for auth in authors:
-        print auth
+    if args.output == 'plaintext':
+        print('Author Commits Inserted Removed Total')
+        for auth in authors:
+            print(auth.report(args.by, sep=' '))
+    elif args.output == 'tex':
+        print('''\documentclass[10pt,border=10pt]{standalone}
+\\usepackage{booktabs}
+\\usepackage{newtxtext}
+\\begin{document}
+\\begin{tabular}{lrrrr}
+\\toprule
+\\emph{Author} & \\emph{Commits} & \\emph{Inserted} & \\emph{Removed} & $\Sigma$ \\\\
+\\midrule''')
+        for auth in authors:
+            print(auth.report(args.by, sep=' & ', suffix = ' \\\\'))
+        print('''\\bottomrule
+\\end{tabular}
+\\end{document}''')
+    elif args.output == 'csv':
+        for auth in authors:
+            print(auth.report(args.by, sep=','))
+    elif args.output == 'alias':
+        for auth in authors:
+            print('%s = %s' % (auth.email, auth.name))
 
 if __name__ == '__main__':
     main()
